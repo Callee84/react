334a1a34fa13b22ba396579dec826f2060c7d8ed
Callee84 <carl.g.holm@gmail.com>
2024-03-06 21:21:20 +0000

    Starting new testproject

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FuncGreet from './components/FuncGreet';
+
+function App() {
+  return (
+    <div className="App">
+      <FuncGreet />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FuncGreet.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+//function FuncGreet() {
+//    return <h1>Well, Hello there!</h1>
+//}
+
+const FuncGreet = () => <h1>Well, hello again</h1>
+
+export default FuncGreet;
